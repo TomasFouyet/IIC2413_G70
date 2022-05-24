50e5e5a8278ee0111e1d0c4a6f51f689686f6683
TomasFouyet <tfouyet@uc.cl>
2022-05-23 22:15:37 -0400

    cambio

--- Sites/consultas/consulta_vuelos_fecha.php
+++ Sites/consultas/consulta_vuelos_fecha.php
@@ -10,9 +10,9 @@ $query = "SELECT a.codigo_aeronave, COUNT(DISTINCT ol.operacion_id)
           FROM operacion_llegada as OL
           JOIN operacion_salida as OS
           ON OS.operacion_id = OL.operacion_id
-          JOIN aeronaves as A
+          JOIN aeronaves1 as A
           ON OS.codigo_aeronave = A.codigo_aeronave
-          WHERE OS.fecha_salida = '$fecha1' AND OL.fecha_llegada = '$fecha2'
+          WHERE OS.fecha_despegue >= '$fecha1' AND OL.fecha_arribo <= '$fecha2'
           GROUP BY A.codigo_aeronave;";
 $result = $db -> prepare($query);
 $result -> execute();
