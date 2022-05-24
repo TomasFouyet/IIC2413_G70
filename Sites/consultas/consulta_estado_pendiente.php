e4c8581d2c9ff46701944d61400415964bb8f0a1
TomasFouyet <tfouyet@uc.cl>
2022-05-23 20:42:39 -0400

    prueba pagina

--- Sites/consultas/consulta_estado_pendiente.php
+++ Sites/consultas/consulta_estado_pendiente.php
@@ -8,7 +8,7 @@
   $query = "SELECT propuesta_vuelo_id, codigo_compania, fecha_envio_propuesta, estado, fecha_salida, fecha_llegada, aerodromo_llegada_id, aerodromo_salida_id FROM propuesta_de_vuelo as PV JOIN tipo_vuelo as TV ON PV.id_extra = TV.id_extra WHERE TV.esatdo = 'pendiente'";
   $result = $db -> prepare($query);
   $result -> execute();
-  $vuelos = $result -> fetchAll()
+  $vuelos = $result -> fetchAll();
 
 ?>
 <table>
@@ -24,7 +24,7 @@
     </tr>
     <?php
     foreach($vuelos as $vuelo){
-        echo "<tr><td>$vuelo[0]</td><td>$vuelo[1]</td><td>$vuelo[2]</td><td>$vuelo[3]</td><td>$vuelo[4]</td><td>$vuelo[5]</td><td>$vuelo[6]</td><td>$vuelo[7]</td></tr>"
+        echo "<tr><td>$vuelo[0]</td><td>$vuelo[1]</td><td>$vuelo[2]</td><td>$vuelo[3]</td><td>$vuelo[4]</td><td>$vuelo[5]</td><td>$vuelo[6]</td><td>$vuelo[7]</td></tr>";
     }
     ?>
 </table>
