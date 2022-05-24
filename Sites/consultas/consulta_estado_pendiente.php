81267394d16923164f8a133619d8f7f26af0b1e8
TomasFouyet <tfouyet@uc.cl>
2022-05-23 20:45:49 -0400

    push pagina

--- Sites/consultas/consulta_estado_pendiente.php
+++ Sites/consultas/consulta_estado_pendiente.php
@@ -5,7 +5,11 @@
 <?php
   require("../config/conexion.php");
 
-  $query = "SELECT propuesta_vuelo_id, codigo_compania, fecha_envio_propuesta, estado, fecha_salida, fecha_llegada, aerodromo_llegada_id, aerodromo_salida_id FROM propuesta_de_vuelo as PV JOIN tipo_vuelo as TV ON PV.id_extra = TV.id_extra WHERE TV.esatdo = 'pendiente'";
+  $query = "SELECT propuesta_vuelo_id,codigo_compania,fecha_envio_propuesta,estado,fecha_salida,fecha_llegada,aerodromo_llegada_id,aerodromo_salida_id \
+            FROM propuesta_de_vuelo as PV
+            JOIN tipo_vuelo as TV
+            ON PV.id_extra = TV.id_extra 
+            WHERE TV.esatdo = 'pendiente';";
   $result = $db -> prepare($query);
   $result -> execute();
   $vuelos = $result -> fetchAll();
