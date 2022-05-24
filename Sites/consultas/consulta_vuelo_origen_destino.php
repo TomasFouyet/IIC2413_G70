ec0f1ed8d089dc199f7da5f98d31dd66bbcd3687
TomasFouyet <tfouyet@uc.cl>
2022-05-23 21:08:41 -0400

    cambio

--- Sites/consultas/consulta_vuelo_origen_destino.php
+++ Sites/consultas/consulta_vuelo_origen_destino.php
@@ -5,21 +5,34 @@
 
   <?php
   require("../config/conexion.php"); #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-
+  $salida = $_POST["destino"]
+  $llegada = $_POST["origen"]
+  $query = "SELECT DISTINCT codigos.codigo_icao,c2.codigo_icao,propuesta_de_vuelo.propuesta_vuelo_id,aerodromos.nombre,a2.nombre
+  FROM codigos1 as codigos, aerodromos, tipo vuelo, propuesta de vuelo, aerodromos as a2, codigos1 as c2
+  WHERE codigo.nombre = aerodromos.nombre
+  and codigo.codigo icao LIKE '%$salida%'
+  and tipo vuelo.aerodromos salida id = aerodromo.aerodromo id
+  and propuesta de vuelo.id extra = tipo vuelo.id extra
+  and tipo vuelo.estado = 'aceptado'
+  and a2.aerodromo id = tipo vuelo.aerodromo llegada id
+  and c2.nombre = a2.nombre
+  and c2.codigo icao LIKE '%$llegada%';";
+  $result = $db -> prepare($query);
+  $result -> execute();
+  $viajes = $result -> fetchAll();
   ?>
 
   <table>
     <tr>
-      <th>ID</th>
-      <th>Nombre</th>
-      <th>Altura</th>
-      <th>Peso</th>
-      <th>Exp Base</th>
-      <th>Tipo</th>
+      <th>Salida</th>
+      <th>LLegada</th>
+      <th>Propuesta de vuelo</th>
+      <th>Aerodromo Salida</th>
+      <th>Aerodromo Llegada</th>
     </tr>
   <?php
-  foreach ($dataCollected as $p) {
-    echo "<tr> <td>$p[0]</td> <td>$p[1]</td> <td>$p[2]</td> <td>$p[3]</td> <td>$p[4]</td> <td>$p[5]</td> </tr>";
+  foreach ($viajes as $viaje) {
+    echo "<tr> <td>$viaje[0]</td> <td>$viaje[1]</td> <td>$viaje[2]</td> <td>$viaje[3]</td> <td>$viaje[4]</td></tr>";
   }
   ?>
   </table>
